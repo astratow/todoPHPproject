6452fac50249794e8561879a7ae9b8cd3dbca2d5
astratow <davno_pora@mail.com>
2020-08-15 22:26:04 +0100

    create.php written

---
+++ create.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <title>Create ToDo List</title>
+</head>
+<body>
+  <form method="post" action="create.php">
+    <p>ToDo title: </p>
+    <input name="todoTitle" type="text">
+    <p>ToDo description: </p>
+    <input name="todoDescription" type="text">
+    <hr>
+    <input name="submit" type="submit" value="submit">
+  </form>
+</body>
+</html>
